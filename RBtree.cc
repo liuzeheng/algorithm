b25791bc8817d70a692dcfea7bdbaccdc1a292f4
lzh <zeheng.liu@gmail.com>
2016-03-18 19:43:10 +0800

    red black tree

--- RBtree.cc
+++ RBtree.cc
@@ -1,6 +1,5 @@
 # include <iostream>
 using namespace std;
-
 class RBtree {
 	public:
 		struct node {
@@ -11,12 +10,7 @@ class RBtree {
     		node * childr;
 		};
 		RBtree(){
-			T.color=1;
-			T.data=0;
-			T.parent=NULL;
-			T.childl=NULL;
-			T.childr=NULL;
-			p_nil=&T;
+			p_nil=NULL;
 			Head=p_nil;
 		}
 		~RBtree(){
@@ -25,24 +19,24 @@ class RBtree {
 		node * search(double data);
 		void insert(double data);
 		void del(double data);
-		void print(){ sub_print(Head);}
-		void sub_print(node * p);
+		void print(){ sub_print(Head,0);}
+		void sub_print(node * p,int depth);
 	private:
 		void del_all(node * p){
-			if (Head==NULL)
+			if (Head==p_nil)
 				return;
 			if (p->childl==p_nil&&p->childr==p_nil){
 				if (Head!=p){
 					if (p->parent->childl==p)
 						p->parent->childl=p_nil;
-					else 
+					else
 						p->parent->childr=p_nil;
 				}
 	//			cout<<"delete data ="<<p->data<<endl;
 			    delete p;
 			}
 			else{
-		        if (p->childl!=p_nil)  
+		        if (p->childl!=p_nil)
 					del_all(p->childl);
 			    if (p->childr!=p_nil)
 					del_all(p->childr);
@@ -55,7 +49,6 @@ class RBtree {
 		void fix_up(node * p);
 		void rotadeR(node * p);
 		void rotadeL(node * p);
-		node T;
 		node *p_nil;
 		node *Head;
 };
@@ -64,31 +57,30 @@ void RBtree::transplant(node *u,node *v){
 		Head = v;
 	else if (u==u->parent->childl)
 		u->parent->childl = v;
-	else 
+	else
 		u->parent->childr = v;
-	if (v!=p_nil)
-		v->parent = u->parent;
+	v->parent = u->parent;
 }
-void RBtree::sub_print(node * p){
-
-	if (p->childl==p_nil&&p->childr==p_nil){
-		cout<<"data ="<<p->data<<" color ="<<p->color<<endl;
+void RBtree::sub_print(node * p,int depth){
+    if (p->childl==p_nil)
+        return;
+	if (p->childl->childl==p_nil&&p->childr->childl==p_nil){
+		cout<<"data ="<<p->data<<" color ="<<p->color<<" depth ="<<depth<<endl;
 	}
 	else{
-		if (p->childl!=p_nil)
-			sub_print(p->childl);
-		cout<<"data ="<<p->data<<" color ="<<p->color<<endl;
-		if (p->childr!=p_nil) 
-			sub_print(p->childr);
-		
+		if (p->childl->childl!=p_nil)
+			sub_print(p->childl,depth+1);
+		cout<<"data ="<<p->data<<" color ="<<p->color<<" depth ="<<depth<<endl;
+		if (p->childr->childl!=p_nil)
+			sub_print(p->childr,depth+1);
+
 	}
 }
-
 void RBtree::fix_up(node * p){
 	if (p->color==0&&p==Head){ //判断是否为根节点红色，若是直接改红修正结束
-		p->color=1;            
+		p->color=1;
 		return;
-	} 
+	}
 	if (p->parent->color==1){ //如果需要修正的节点的上一个节点为黑则修正结束
 		return;
 	}
@@ -102,7 +94,7 @@ void RBtree::fix_up(node * p){
 				cout<<"p.parent.parent.data ="<<p->parent->parent->data<<endl;
 		*/	}
 		//	rotadeL(p->parent->parent);
-			if (p->parent->parent->childl->color==0){//如果叔叔节点为红	
+			if (p->parent->parent->childl->color==0){//如果叔叔节点为红
 				p->parent->parent->color=0;//祖父变红
 				p->parent->parent->childl->color=1;//叔叔变黑
 				p->parent->color=1;//父亲变黑
@@ -130,8 +122,86 @@ void RBtree::fix_up(node * p){
 			}
 		}
 }
-
 void RBtree::delete_fix_up(node * x){
+    while(x!=Head){
+        if (x==x->parent->childl){
+            if (x->parent->childr->color==0){
+                rotadeL(x->parent);
+                x->parent->color = 0;
+                x->parent->parent->color = 1;
+            }
+            node *w =x->parent->childr;
+            if (w->childr->color==1&&w->childl->color==1){
+                w->color = 0;
+                if (w->parent->color ==0){
+                    w->parent->color =1;
+                    x = Head;
+                }else{
+                    x= w->parent;
+                }
+            }else if (w->childr->color ==1&&w->childl->color==0){
+                 rotadeR(w);
+                 w->color = 0;
+                 w->parent->color =1;
+                 if (x->parent->color ==0){
+                    x->parent->color =1;
+                    x = Head;
+                 }else{
+                      x=x->parent;
+                 }
+            }else{
+                rotadeL(x->parent);
+                w->childr->color = 1;
+                if (x->parent->color ==1){
+                     x = Head;
+                }else{
+                    x->parent->color =1;
+                    w->color = 0;
+                    x = Head;
+                }
+            }
+        }else{
+            if (x->parent->childl->color==0){
+                rotadeR(x->parent);
+                x->parent->color = 0;
+                x->parent->parent->color = 1;
+            }
+            node *w =x->parent->childl;
+            if (w->childr->color==1&&w->childl->color==1){
+                w->color = 0;
+                if (w->parent->color ==0){
+                    w->parent->color =1;
+                    x = Head;
+                }else{
+                    x= w->parent;
+                }
+            }else if (w->childl->color ==1&&w->childr->color==0){
+                 rotadeL(w);
+                 w->color = 0;
+                 w->parent->color =1;
+                 if (x->parent->color ==0){
+                    x->parent->color =1;
+                    x = Head;
+                 }else{
+                      x=x->parent;
+                 }
+            }else{
+                rotadeR(x->parent);
+                w->childl->color = 1;
+                if (x->parent->color ==1){
+                     x = Head;
+                }else{
+                    x->parent->color =1;
+                    w->color = 0;
+                    x = Head;
+                }
+
+             }
+        }
+
+    }
+    x->color = 1;
+
 }
 void RBtree::rotadeR(node * p){//右旋，旋转后该节点成为右儿子
 	if (p==Head){
@@ -139,47 +209,37 @@ void RBtree::rotadeR(node * p){//右旋，旋转后该节点成为右儿子
 	}
 	else if (p->parent->childl==p){
 		p->parent->childl = p->childl;
-		if (p->childl!=p_nil)
-			p->childl->parent = p->parent;
+		p->childl->parent = p->parent;
 	}
 	else {
 		p->parent->childr = p->childl;
-		if (p->childl!=p_nil)
-			p->childl->parent = p->parent;
+		p->childl->parent = p->parent;
 	}
-
 	node * temp =p->childl;
 	p->childl = p->childl->childr;
-	if (p->childl!=p_nil)
-		p->childl->parent = p;
+	p->childl->parent = p;
 	temp->childr = p;
 	p->parent = temp;
-	
-}
 
+}
 void RBtree::rotadeL(node * p){//左旋，旋状转后该节点成为左儿子
 	if (p ==Head){
 		Head = p->childr;
 	}
 	else if (p->parent->childl==p){
 		p->parent->childl = p->childr;
-		if (p->childr!=p_nil)
-			p->childr->parent = p->parent;
+		p->childr->parent = p->parent;
 	}
 	else{
 		p->parent->childr = p->childr;
-		if (p->childr!=p_nil)
-			p->childr->parent = p->parent;
+		p->childr->parent = p->parent;
 	}
-
 	node * temp = p->childr;
 	p->childr = p->childr->childl;
-	if (p->childr!=p_nil)
-		p->childr->parent = p;
-	temp->childl = p; 	
-	p->parent = temp; 
+	p->childr->parent = p;
+	temp->childl = p;
+	p->parent = temp;
 }
-
 RBtree::node * RBtree::search(double data){//寻找节点
 	 node * p =Head;
 	 while(p->data!=data){
@@ -190,44 +250,53 @@ RBtree::node * RBtree::search(double data){//寻找节点
 	}
 	return p;
 }
-
 void RBtree::insert(double data){//插入节点
 	node * p =Head;
 	if (Head==p_nil){//插入第一个节点
-		node *a=new node {1,data,0,p_nil,p_nil};
+        node *T1 = new node {1,0,NULL,NULL,NULL};
+        node *T2 = new node {1,0,NULL,NULL,NULL};
+		node *a=new node {1,data,0,T1,T2};
+        T1->parent = a;
+        T2->parent = a;
 		Head = a;
 	}
 	else{
-			while((p->childl!=p_nil||p->data<data)&&(p->childr!=p_nil||p->data>data)){//找到合适的叶子节点
+			while((p->childl->childl!=p_nil||p->data<=data)&&(p->childr->childl!=p_nil||p->data>data)){//找到合适的叶子节点
 				if (p->data>data)
 					p=p->childl;
-				else 
+				else
 					p=p->childr;
 			}
-			
-			node *a= new node {0,data,p,p_nil,p_nil};
+            node *T1 = new node {1,0,NULL,NULL,NULL};
+            node *T2 = new node {1,0,NULL,NULL,NULL};
+			node *a= new node {0,data,p,T1,T2};
+            T1->parent = a;
+            T2->parent = a;
 
-			if (p->data>data)//插入数据
-				p->childl=a;
-			else
+			if (p->data>data){//插入数据
+				delete p->childl;
+                p->childl=a;
+            }
+            else{
+                delete p->childr;
 				p->childr=a;
+            }
 			fix_up(a); //修正红黑树
 		}
 }
-
 void RBtree::del(double data){
 	node * del_p = search(data);
 	bool color = del_p->color;
 	node * x;
-	if (del_p->childl==p_nil){
-		x = del_p->childr; 
+	if (del_p->childl->childl==p_nil){
+		x = del_p->childr;
 		transplant(del_p,del_p->childr);
-	}else if (del_p->childr==p_nil){
+	}else if (del_p->childr->childl==p_nil){
 		x = del_p->childl;
 		transplant(del_p,del_p->childl);
 	}else {
 		node * y =del_p->childr;
-		while(y->childl!=p_nil)
+		while(y->childl->childl!=p_nil)
 			 y=y->childl;
 		color = y->color;
 		x = y->childr;
@@ -247,26 +316,20 @@ void RBtree::del(double data){
 		delete_fix_up(x);
 	delete del_p;
 }
-
 int main(){
-	double a[]={2,5,3,4,3,5,7,9,1,6,10};
+	double a[]={2,5,3,4,3,7,5,9,1,6,10};
 	RBtree RB;
 	for (int i=0;i<11;i++){
+        cout<<"\n insert "<<a[i]<<endl;
 		RB.insert(a[i]);
 		RB.print();
 	}
-	for (int i=0;i<10;i++){
+	for (int i=0;i<11;i++){
+        cout<<"\n delete "<<a[i]<<endl;
 		RB.del(a[i]);
 		RB.print();
 	}
-	return 0;	
+	return 0;
 }
-
-
-
-
-
-
-
 
 
